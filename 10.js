0199d3c408c21167e0ed4f595ff09c18362421bb
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-05-26 08:00:03 -0400

    Actualizando proyecto

---
+++ 10.js
@@ -0,0 +1,34 @@
+Vue.component('padre', {
+  template: /*template*/ `
+    <div>
+      <h1>Número {{$store.state.numero}}</h1>
+    </div>
+
+  `
+});
+const store = new Vuex.Store({
+  state: {
+    'numero': 0
+  },
+  'mutations': {
+    increment(state) {
+      state.numero++;
+    }
+  }
+});
+Vue.component('hijo', {
+  template: /*template*/ `
+  <div>
+    <button @click="$store.commit('increment')">+</button>
+  </div>
+  `
+});
+
+new Vue({
+  'el': '#app',
+  // store: store,
+  store,
+  data: {
+    'title': 'Vue 10 - Introducción a VueX'
+  }
+});
